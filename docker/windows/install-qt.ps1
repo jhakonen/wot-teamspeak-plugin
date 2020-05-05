d4f2bcab1ba98eeb83261a4d04451d7b22b1e288
Janne Hakonen <janne.hakonen@iki.fi>
2020-05-05 21:25:30 +0300

    Install Qt and Python

---
+++ docker/windows/install-qt.ps1
@@ -0,0 +1,6 @@
+Invoke-WebRequest `
+    'http://master.qt.io/online/qtsdkrepository/windows_x86/desktop/qt5_5123/qt.qt5.5123.win64_msvc2015_64/5.12.3-0-201904161237qtbase-Windows-Windows_10-MSVC2015-Windows-Windows_10-X86_64.7z' `
+    -OutFile 'qtbase.7z' `
+    -UseBasicParsing
+
+Start-Process 'c:\program files\7-zip\7z.exe' -Wait -NoNewWindow -ArgumentList 'x "C:\build\qtbase.7z" -oqt'
