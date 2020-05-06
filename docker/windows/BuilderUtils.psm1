0425d97cff34ae7188b9cc59608b7f07538de569
Janne Hakonen <janne.hakonen@iki.fi>
2020-05-06 23:27:36 +0300

    Revert "Try with different exit check"
    This reverts commit 6bab4f02557514c72c6b25cab30e4b73bb510d92.

--- docker/windows/BuilderUtils.psm1
+++ docker/windows/BuilderUtils.psm1
@@ -4,8 +4,8 @@ function Invoke-NativeProgram {
     )
     Write-Host $ScriptBlock
     & $ScriptBlock 2>&1 >$env:TEMP\out.log
-    if (!$?) {
+    if ($LASTEXITCODE -ne "0") {
         Get-Content $env:TEMP\out.log
-        throw "Execution failed"
+        throw "Execution failed with exit code: $LASTEXITCODE"
     }
 }
